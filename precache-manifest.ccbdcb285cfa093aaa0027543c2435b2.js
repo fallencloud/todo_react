f8e916ab4b0788d81f771094d016068202e48d51
fallencloud <sharinavjones@gmail.com>
2019-05-09 18:06:12 -0400

    Updates

---
+++ precache-manifest.ccbdcb285cfa093aaa0027543c2435b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad15a11877ad8c52e72ffbca83e9134",
+    "url": "/todo_react/index.html"
+  },
+  {
+    "revision": "d251a7309fbf15e4985c",
+    "url": "/todo_react/static/css/2.058a5f6b.chunk.css"
+  },
+  {
+    "revision": "964432d23408bc1fc33a",
+    "url": "/todo_react/static/css/main.5547a834.chunk.css"
+  },
+  {
+    "revision": "d251a7309fbf15e4985c",
+    "url": "/todo_react/static/js/2.0edd2673.chunk.js"
+  },
+  {
+    "revision": "964432d23408bc1fc33a",
+    "url": "/todo_react/static/js/main.6bb99646.chunk.js"
+  },
+  {
+    "revision": "8ae4c28430c1dff67705",
+    "url": "/todo_react/static/js/runtime~main.8f972959.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/todo_react/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/todo_react/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/todo_react/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/todo_react/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/todo_react/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
